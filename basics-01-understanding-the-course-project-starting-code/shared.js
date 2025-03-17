6f971b18ec61cfacf350ee4308477041e4fbf53d
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-17 10:26:03 -0700

    Add modal JS; add mobile CSS and HTML; add mobile button HTML, CSS

--- basics-01-understanding-the-course-project-starting-code/shared.js
+++ basics-01-understanding-the-course-project-starting-code/shared.js
@@ -1,4 +1,20 @@
 var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButton = document.querySelectorAll(".plan button");
 
-console.dir(selectPlanButton);
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", closeModal);
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+};
