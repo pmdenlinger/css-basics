1530d9176e4a18ea1351e07cc0a8a451c2db2bc8
Paul Denlinger <pmdenlinger@icloud.com>
2025-03-19 10:24:28 -0700

    Correct styling and markup so that hamburger icon appears in navigation bar

--- basics-01-understanding-the-course-project-starting-code/shared.js
+++ basics-01-understanding-the-course-project-starting-code/shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButton = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (var i = 0; i < selectPlanButton.length; i++) {
   selectPlanButton[i].addEventListener("click", function () {
@@ -10,11 +12,19 @@ for (var i = 0; i < selectPlanButton.length; i++) {
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 
 modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
-};
+}
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
